3622e7122e0c7e9a3416fba0f6b19b5ed8293743
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2019-11-02 20:35:00 -0300

    CORS implemented

--- src/main/java/br/com/debpay/Main.java
+++ src/main/java/br/com/debpay/Main.java
@@ -4,6 +4,10 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URI;
 
@@ -22,10 +26,27 @@ public class Main {
     static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         final ResourceConfig rc = new ResourceConfig().packages("br.com.debpay.Controllers");
+        rc.register(new CORSFilter());
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /* Used for tests purposes only - Should be disabled or better implemented on the future*/
+    @Provider
+    public static class CORSFilter implements ContainerResponseFilter {
+
+        @Override
+        public void filter(ContainerRequestContext request,
+                           ContainerResponseContext response) throws IOException {
+            response.getHeaders().add("Access-Control-Allow-Origin", "*");
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                    "origin, content-type, accept, authorization");
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        }
     }
 
     /**
