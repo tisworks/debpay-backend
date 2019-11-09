ec6a790e66eb5d26d732179d5307fb288384dc73
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2019-11-08 22:20:01 -0300

    Maven binary creation

--- src/main/java/br/com/debpay/Container.java
+++ src/main/java/br/com/debpay/Container.java
@@ -7,8 +7,7 @@ import br.com.debpay.infrastructure.SQLDatabase;
 import br.com.debpay.services.*;
 
 public class Container {
-  private static final SQLDatabase SQLite =
-      new SQLDatabase("org.sqlite.JDBC", "jdbc:sqlite:test.db");
+  private static SQLDatabase SQLite;
   private static final String salt = "JoseDaSilva";
 
   private Container() {}
@@ -21,7 +20,9 @@ public class Container {
     return new OperationService(new OperationDAO(SQLite));
   }
 
-  public static IContactService getContactService() {
-    return new ContactService(new ContactDAO(SQLite));
+  public static IContactService getContactService() { return new ContactService(new ContactDAO(SQLite)); }
+
+  public static void setDataBasePath(String path) {
+    SQLite = new SQLDatabase("org.sqlite.JDBC", "jdbc:sqlite:" + path);
   }
 }
